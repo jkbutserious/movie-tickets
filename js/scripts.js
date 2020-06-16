3425963e8f130e3f57f2fb65ab1e3f74da25d153
Jeff Kim <kim2jy@hotmail.com>
2020-06-16 11:38:52 -0700

    Add user interface function to pass user data into the Ticket object
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,26 @@ function Tickets(movieName, showTime, age, price) {
 
 Tickets.prototype.ticketPrice = function(inputtedShowTime, inputtedAge) {
   let finalPrice = 15;
-  if (inputtedShowTime <= 1600 || inputtedAge >= 65) {
+  if (inputtedShowTime < 1600 || inputtedAge > 65) {
     finalPrice -= 5;
   }
-  else if (inputtedShowTime <= 1600 && inputtedAge >= 65) {
+  else if (inputtedShowTime === 1600 && inputtedAge === 65) {
     finalPrice -= 10;
   }
   return finalPrice;
   this.price.push(finalPrice);
 }
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#movieTicket").submit(function(event) {
+    event.preventDefault();
+    const userMovieName = $("input:radio[name=movie-name]:checked").val();
+    const userShowTime = parseInt($("input:radio[name=showtime]:checked").val());
+    const userAge = parseInt($("input:radio[name=age]:checked").val());
+    let newTicket = new Tickets(userMovieName, userShowTime, userAge);
+
+    newTicket.price = newTicket.ticketPrice(userShowTime, userAge);
+    console.log(newTicket);
+  });
+})
