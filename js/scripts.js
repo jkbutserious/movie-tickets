7a3bda5cca744b87cadaf3cefd504d43f9cfe1fd
Jeff Kim <kim2jy@hotmail.com>
2020-06-16 10:52:04 -0700

    Add prototype for calculating ticket price
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 // Business Logic for Tickets
-function Tickets(movieName, showTime, age) {
+function Tickets(movieName, showTime, age, price) {
   this.movieName = movieName,
   this.showTime = showTime,
   this.age = age,
+  this.price = price
+}
+
+Tickets.prototype.ticketPrice = function(inputtedShowTime, inputtedAge) {
+  let finalPrice = 15;
+  if (inputtedShowTime <= 1600 || inputtedAge >= 65) {
+    finalPrice -= 5;
+  }
+  else if (inputtedShowTime <= 1600 && inputtedAge >= 65) {
+    finalPrice -= 10;
+  }
+  return finalPrice;
+  this.price.push(finalPrice);
 }
